ca3267923265ef2a972d9fa6be4265917d875933
bhargavkundrapu <kundrapubhargav@gmail.com>
2025-05-23 05:54:25 +0000

    viteconfigjschanged

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    allowedHosts: [
+      'bhargavor2qcrjsivddczf.drops.nxtwave.tech',
+      'localhost',
+      '127.0.0.1',
+    ],
+    // Optional: specify port if needed
+    port: 3000,
+  },
 })
