53045763e790d1f73ca4f46332178bd3058c1c99
chaitu347 <chaitu347347@gmail.com>
2025-05-27 01:17:57 +0530

    footer added

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -15,63 +15,31 @@ const About = () => {
   const [sectionRef, isInView] = useIntersectionObserver();
   return (
     <section ref={sectionRef}  id="about" className="h-dvh w-screen overflow-x-hidden stack">
-      <div className="stacking-section h-[100%] w-[100%] bg-carbon-mist flex flex-col xl:flex-row items-center justify-center">
+      <div className="stacking-section h-[100%] w-[100%] bg-[#000] flex flex-col xl:flex-row items-center justify-center">
         {/* left part */}
-        <div className="w-[100%] h-[50%] lg:w-[50%] lg:h-[100%] flex flex-col items-center justify-end">
-          {/* first text */}
-          <div className="w-[100%] h-[20%] mb-2 flex items-center">
-            <div>
-              <BlurText
-                text="Learning"
-                delay={150}
-                animateBy="words"
-                direction="top"
-                className="text-6xl sm:text-9xl text-alpine-frost pl-5 font-robert-medium font-extrabold"
-              />
-            </div>
-          </div>
-          {/* second text */}
-          <div className="w-[100%] h-[20%] flex mb-2 items-center">
-            <div>
-              <BlurText
-                text="Building"
-                delay={150}
-                animateBy="words"
-                direction="top"
-                className="text-6xl sm:text-9xl text-blue-tint font-robert-medium font-extrabold ml-15 sm:ml-25"
-              />
-            </div>
-          </div>
-          {/* third text */}
-          <div className="w-[100%] h-[20%] flex items-center mb-2">
-            <div>
-              <BlurText
-                text="Shipping"
-                delay={150}
-                animateBy="words"
-                direction="top"
-                className="text-6xl sm:text-9xl text-alpine-frost font-robert-medium font-extrabold ml-25 sm:ml-45"
-              />
-            </div>
-          </div>
+        <div className="brightness-50 order-1 md:order-2 w-[100%] h-[50%] lg:w-[50%] lg:h-[80%] sm:h-[90%] flex flex-col items-center justify-end">
+          <img  src="https://res.cloudinary.com/dnbnst2wn/image/upload/v1748281650/photo_jkc3j1.jpg" alt="profilepic" />     
+          
         </div>
 
         {/* right part */}
-        <div className="w-[100%] h-[50%] sm:w-[50%] sm:h-[100%] ">
+        <div className="order-2 ml-7 md:order-1 w-[100%] h-[50%] sm:w-[50%] sm:h-[100%] ">
           {/* main container */}
+          
           <div className="h-full w-full p-5">
             {/* about me text */}
             <div className=" flex items-center justify-start h-[20%]">
               <div>
                 <GradientText
                   colors={[
-                    "#a1a1a6",
-                    "#7d7d81",
-                    "#121212",
-                    "#a1a1a6",
-                    "#7d7d81",
+                    "#4c00ff",
+                    "#8000ff",
+                    "#f79d00",
+                    
+                    "#e8b31e",
+                    "#b03a00",
                   ]}
-                  animationSpeed={5}
+                  animationSpeed={9}
                   showBorder={false}
                   className="custom-class font-robert-medium text-6xl sm:text-8xl font-bold"
                 >
@@ -83,19 +51,22 @@ const About = () => {
             {/* whole about me text */}
             <div className="flex-col items-start justify-start mt-10 h-[70%] pr-5">
               <p className="font-robert-medium text-sm sm:text-2xl text-metallic-gray" >
-                Namaste, I'm{" "}
-                <span className="text-blue-tint">Vishvjeet Rana</span> — a Full
-                Stack Web Developer who enjoys building web applications with
-                clean design and smooth functionality. I specialize in crafting
-                seamless digital experiences by blending creativity with
-                efficient problem-solving. Occasionally I work with Three js for
-                giving 3D and realistic feel to websites.
-              </p>
+                Greetings! I'm{" "}
+                <span className="text-blue-tint">Bhargav Kundrapu</span> a Digital Experience Architect who thrives on transforming wild ideas into interactive realities.
+              </p><br/>
               <p className="font-robert-medium text-sm sm:text-2xl text-metallic-gray">
                 I'm diving into the world of{" "}
                 <span className="text-blue-tint">Artificial Intelligence</span>{" "}
                 to add smarter features to websites. It's all about making web
-                apps more helpful and responsive in simple ways.
+                apps more helpful and responsive in simple ways.<br/><br/>
+                <span className="text-blue-tint">My philosophy is simple-</span>{" "}
+                every line of code should tell a story, every interaction should spark joy, and every application should feel less like software and more like magic. Whether I'm optimizing database queries at 2 AM or sketching wireframes over morning coffee, I'm always chasing that perfect moment when technology becomes invisible and user delight takes center stage.
+                <br/><br/>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                
+                <span className="text-white">-Bhargav Kundrapu </span>{" "}
+                <br/><br/><br/>
               </p>
             </div>
           </div>
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,6 +2,9 @@ import React, { useRef, useState, useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { useIntersectionObserver } from "../useIntersectionObserver.jsx";
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoGithub } from "react-icons/io";
+import { IoLogoLinkedin } from "react-icons/io5";
 import "./Footer.css";
 import "../../App.css";
 
@@ -48,12 +51,58 @@ const Footer = ({ isActiveSection = false }) => {
   };
 
   return (
-    <section ref={sectionRef} id="footer" className="skills-container stack min-h-screen bg-gradient-to-br from-purple-50 to-pink-100 transition-all duration-1000">
-      <div>
-      <img src="https://res.cloudinary.com/dnbnst2wn/image/upload/v1748269848/32399-3840x2160-desktop-4k-avengers-wallpaper_o7akut.jpg" alt="footer-image"  />
-      <div className="gradient-overlay-left-footer"></div>
-      <div className="gradient-overlay-top-footer"></div>
+    <section ref={sectionRef} id="footer" className="footer-container h-dvh w-screen overflow-x-hidden stack">
+       <div className="h-full w-full  flex items-center justify-center">
+        {/* main container */}
+        <div className="h-full w-full flex flex-col items-center justify-center">
+          {/* the main text */}
+          <div className="h-[60%] w-[80%] flex flex-col items-center justify-center pt-15 gap-5">
+            <p className="font-robert-small text-8xl sm:text-9xl font-extrabold text-[#50d71e]">
+             What Next...
+            </p>
+            <p className="font-robert-medium text-[#f79d00] text-3xl sm:text-6xl font-bold">
+             Looking For a Creative Developer?
+            </p>
+            <p className="font-robert-medium text-md font-semibold text-[#b695f9]">
+              The web is my canvas, JavaScript is my brush, and user satisfaction is my masterpiece.
+            </p>
+          </div>
+
+          {/* social icons */}
+          <div className="h-[30%] w-[100%]  flex items-center justify-center">
+            <div className="h-[30%] w-[70%] sm:h-[35%] sm:w-[20%] bg-alpine-frost rounded-4xl flex items-center justify-around">
+              <div className="h-[90%] rounded-4xl w-[20%] text-5xl flex items-center justify-center text-carbon-mist hover:scale-125 duration-200 transition-all hover:text-blue-600 transform hover:-translate-y-3 cursor-pointer">
+                <a
+                  href="https://x.com/RVishvjeet_/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <FaXTwitter />
+                </a>
+              </div>
+              <div className="h-[90%] rounded-4xl w-[20%] text-5xl flex items-center justify-center text-carbon-mist hover:scale-125 duration-200 transition-all hover:text-blue-600 transform hover:-translate-y-3 cursor-pointer">
+                <a
+                  href="https://github.com/Vishvjeet-Rana/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
+              <div className="h-[90%] rounded-4xl w-[20%] text-5xl flex items-center justify-center text-carbon-mist hover:scale-125 duration-200 transition-all hover:text-blue-600 transform hover:-translate-y-3 cursor-pointer">
+                <a
+                  href="https://www.linkedin.com/in/vishvjeet-rana/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      
     </section>
   );
 };
